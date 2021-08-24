dea95c6d932ce0be59955ccb829f57c8fc6dcc97
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-24 16:03:25 -0700

    have Auth.js have rendered Email and Password inputs just need to grab component to add to App.js

--- src/Auth.js
+++ src/Auth.js
@@ -17,11 +17,34 @@ class Authorization extends Component {
             },
             this.props.type
         );
+        this.props.setToken(token);
         
     }
 
     render() { 
-        return (  );
+        return ( 
+            <>
+            <h1>{this.getType()}</h1>
+            <form onSubmit={this.handleSubmit}>
+                <div className="form">
+                    <label>Email: </label>
+                    <input
+                        type='email'
+                        placeholder='Email'
+                        onChange={(e) => this.setState({ email: e.target.value })} />
+                </div>
+                <div className="form">
+                    <label>Email: </label>
+                    <input
+                        type='password'
+                        placeholder='Password'
+                        onChange={(e) => this.setState({ password: e.target.value })} />
+                </div>
+                <button>{this.getType()}</button>
+            </form>
+            </>
+
+         );
     }
 }
  
