a9a5165754be0632ea9b4cfa65225cfd8ad64a9b
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-26 11:58:14 -0700

    todo list is showing for user

--- src/Todos.js
+++ src/Todos.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
-// import { getTodos, createTodos, updateTodos } from './Utils.js';
+import { getTodos } from './Utils.js';
 
 class ToDoList extends Component {
     state = { 
-        to_do: []
-     }
+        to_do: [],
+        newTo_do: ''
+     };
+
+     componentDidMount = () => {
+         this.fetchTodos();
+         
+     };
+
+     fetchTodos = async () => {
+         const data = await getTodos(this.props.token);
+         console.log(data);
+         this.setState({ to_do: data });
+     };
+
+     handleSubmit = async
+
     render() { 
         return ( 
             <>
-            <h1>HELLO</h1>
+            <h1>To Do List</h1>
+            <section className='list'>
+                {this.state.to_do.map((item) => (
+                    <div className='todos' key={item.id}>
+                        <input 
+                        type='checkbox' 
+                        checked={item.completed}
+                       >
+
+                        </input>
+                        <label>{item.to_do}</label>
+                    </div>
+                ))}
+            </section>
             </>
          );
     }
